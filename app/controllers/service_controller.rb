a1af2cd39029f6e50e6a1f97edbc5a921ccd254a
Your Name <you@example.com>
2010-12-04 15:29:16 -0500

    fixed service postback

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -1,12 +1,13 @@
 class ServiceController < ApplicationController
+  protect_from_forgery :except => :github
 
   def github
     # get the params and create a commit entry
     payload = JSON.parse(params[:payload])
     project = Project.find(params[:service_id])
     ## add new build and commit
-    build = Build.new
-    build.commits << GitHub.parse(payload)
+    Service::Github.parse(project, payload)
+    project.build!
     render :text => "OK", :status => 200
   end
 
