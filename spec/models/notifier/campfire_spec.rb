0af24c5ea8b4f903b550bbca126d9472ce82eb9c
Andy Holman <andy@conspyre.com>
2010-12-04 22:13:50 -0500

    Working on tests.

--- spec/models/notifier/campfire_spec.rb
+++ spec/models/notifier/campfire_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Notifier::Campfire do
+
+  before(:each) do
+    @build = Fabricate(:build, :state => :building)
+  end
+
+  it 'should send a failed message' do
+    #Notifier::Campfire.should_receive(:send_failed)
+    Tinder::Campfire.should_receive(:new)
+    @build.build_failed!
+  end
+
 end
