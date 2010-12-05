ce3ba742201432401e1f2c42652bab913bd6fc2d
Andy Holman <andy@conspyre.com>
2010-12-04 22:08:08 -0500

    Cleaned up notifier code for campfire.

--- app/models/notifier/campfire.rb
+++ app/models/notifier/campfire.rb
@@ -9,29 +9,26 @@
   embedded_in :project, :inverse_of => :campfire
 
   def send_failed(build)
-    room = build_room
     link = build_url(build, :host => APP_CONFIG[:domain])
-    room.speak "[#{build.project.name}] Build #{build.id} failed. #{link}"
-    room.leave
+    send_message("[#{build.project.name}] Build #{build.id} failed. #{link}")
   end
 
   def send_success(build)
-    room = build_room
     link = build_url(build, :host => APP_CONFIG[:domain])
-    room.speak "[#{build.project.name}] Build #{build.id} successful. #{link}"
-    room.leave
+    send_message("[#{build.project.name}] Build #{build.id} successful. #{link}")
   end
 
   def post_message?
     !token.empty? && !room.empty? && !subdomain.empty?
   end
-  
-  def build_room
+ 
+  def send_message(message)
     campfire = Tinder::Campfire.new(self.subdomain, :token => self.token, :ssl => self.ssl)
     room = campfire.find_room_by_name(self.room)
-    room
+    room.speak(message)
+    room.leave
   end
-  private :build_room 
+  private :send_message 
 
   def self.icon
     "notifications/campfire.png"
