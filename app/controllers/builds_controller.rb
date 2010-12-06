6735f79a3096e090a8269c2ad30fdf23b7183d27
Andy Holman <andy@conspyre.com>
2010-12-05 21:20:30 -0500

    Working on some javascript changes as well as per project views to make way for public/private builds.

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,33 +1,39 @@
 class BuildsController < ApplicationController
 
-  def destroy
-    @build = Build.find(params[:id])
-    @build.destroy
+  #before_filter :find_build, :except => [:latest_builds, :latest_builds_by_project]
 
+  def destroy
+    @build.destroy
     respond_to do |format|
       format.html { redirect_to(root_url) }
     end
-
   end
 
   def show
-    @build = Build.find(params[:id])
   end
 
   def build_status
-    @build = Build.find(params[:id])
     render :partial => "build", :layout => false, :locals => {:build => @build}
   end
 
   def latest_builds
-    project = params[:project].blank? ? nil : params[:project]
-    if params[:id] == 'undefined'
-      @builds = Build.desc(:build_num)
-    else
-      build = Build.find(params[:id])
-      @builds = Build.where(:build_num.gt => build.build_num).desc(:created_at)
+    @builds = Build.desc(:build_num)
+
+    begin
+      if params[:id]
+        project = Project.where(:slug => params[:id]).first
+        @builds.where(:project_id => project.id)
+      end
+    rescue
+      project = nil
     end
-    render :layout => false
+
+    project.nil? ? render(:text => "Not Found", :status => 404) :  render(:layout => false)
   end
+
+  def find_build
+    @build = Build.find(params[:id])
+  end
+  private :find_build
 
 end
