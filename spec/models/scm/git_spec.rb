f12efe7e0b0d43b0f00168e8873b85548aeafa88
Andy Holman <andy@conspyre.com>
2010-12-12 19:51:57 -0500

    Added some tests for the git scm model.

---
+++ spec/models/scm/git_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Bob::Git do
+
+  let(:build) { Fabricate(:build) }
+
+  before(:each) do
+    build_testrepo
+    Runner.stub!(:run).and_return('Hi\nthere')
+  end
+
+  after(:each) do
+    remove_testrepo
+    FileUtils.rm_rf(build.build_dir)
+  end
+
+  it 'should build a commit' do
+    build.should_receive(:save)
+    build.commits.count.should eql 4
+    Bob::Git.build_commit(build)
+    build.commits.count.should eql 5
+  end
+
+  it 'should detect the branch on commits' do
+    command = Bob::Git.checkout(build)
+    command.should eql "git clone --branch #{build.latest_commit.branch} --depth 1 #{build.project.scm_path} #{build.build_dir}"
+  end
+
+end
